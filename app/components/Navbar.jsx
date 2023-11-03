233adeb6277688670a43e967e9cd84c49bb77c09
Daniel <cawasdn@gmail.com>
2023-11-03 15:07:58 +0800

    Navbar and banner added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,18 @@
+import Image from 'next/image'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className=''>
+      <div className='flex justify-center sm:justify-start items-center bg-banner-bg w-full h-48 bg-cover bg-no-repeat object-cover bg-center sm:relative'>
+        <div className='backdrop bg-white p-8 rounded-md sm:left-16 sm:absolute sm:w-[700px]'>
+          <h1 className='text-3xl text-[#3F2107] font-bold sm:text-5xl sm:my-2 sm:font-normal'>
+            The Big Cup .
+          </h1>
+          <p>Where taste meets imagination</p>
+        </div>
+      </div>
+      <nav className='bg-slate-400'>Navbar</nav>
+    </div>
+  )
 }
 export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,22 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'banner-bg': "url('../public/banner.jpg')",
+
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic':
+        //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
       },
     },
   },
