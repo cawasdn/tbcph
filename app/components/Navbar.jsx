977533abb018d91c985109e92efd8878621daccf
Daniel <cawasdn@gmail.com>
2023-11-03 16:23:21 +0800

    Fix

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import Image from 'next/image'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
     <div className=''>
-      <div className='flex justify-center sm:justify-start items-center bg-banner-bg w-full h-48 bg-cover bg-no-repeat object-cover bg-center sm:relative'>
-        <div className='backdrop bg-white p-8 rounded-md sm:left-16 sm:absolute sm:w-[700px]'>
-          <h1 className='text-3xl text-[#3F2107] font-bold sm:text-5xl sm:my-2 sm:font-normal'>
+      <div className='flex justify-center  items-center bg-banner-bg w-full h-48 bg-cover bg-no-repeat object-cover bg-center sm:relative sm:justify-start'>
+        <div className='backdrop bg-white p-8 rounded-md sm:left-16 sm:absolute lg:w-[600px]'>
+          <h1 className='text-3xl text-primary font-bold sm:text-5xl sm:my-2 sm:font-normal'>
             The Big Cup .
           </h1>
           <p>Where taste meets imagination</p>
         </div>
       </div>
-      <nav className='bg-slate-400'>Navbar</nav>
+      <nav className='bg-slate-400 sm:bg-orange-500 md:bg-yellow-700 lg:bg-green-100 flex justify-end p-2 sm:p-4 sm:justify-between '>
+        <ul className='hidden sm:flex sm:space-x-5 sm:pl-20 '>
+          <li>HOME</li>
+          <li>ABOUT US</li>
+        </ul>
+        <div className='sm:hidden'>
+          <AiOutlineMenu size={32} />
+        </div>
+      </nav>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,34 @@ module.exports = {
         // 'gradient-conic':
         //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+
+        // xs: '320px',
+        // sm: '640px',
+        // md: '961px',
+        // lg: '1024px',
+        // xl: '1281px',
+      },
       colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
+        primary: '#3F2107',
+        secondary: '#9B4931',
+        tertiary: '#8EA3B6',
+        four: '#1B3C5F',
+        fifth: '#106756',
       },
     },
   },
